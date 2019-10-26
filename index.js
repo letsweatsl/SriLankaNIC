50ffba3d16c6f2db393feb718c615c2d21756201
letsweatsl <57046945+letsweatsl@users.noreply.github.com>
2019-10-27 01:01:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
     next();
   });
 
-  app.listen(8080 ,()=>console.log("Express Server Sratrted"));
+  app.listen(8080 ,()=>console.log("Express Server Sratrted!"));
 
   app.get('/nic',(req,res)=>{
 
